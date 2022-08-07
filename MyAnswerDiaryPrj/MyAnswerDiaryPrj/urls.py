53efc0312340e15337dc045efdf81c02aa72888b
KimMinheee <99alsgmlalsl@naver.com>
2022-08-08 03:37:42 +0900

    [Feat] : create diary

--- MyAnswerDiaryPrj/MyAnswerDiaryPrj/urls.py
+++ MyAnswerDiaryPrj/MyAnswerDiaryPrj/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path
 from DiaryApp import views
 from AccountsApp import views as ac_views
+from DiaryApp import views as diary_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -11,4 +12,5 @@ urlpatterns = [
     path('login/', ac_views.login, name='login'),
     path('logout/', ac_views.logout, name='logout'),
     path('signup/', ac_views.signup, name='signup'),
+    path('create_diary/',diary_views.createDiary, name='create_diary'),
 ]
